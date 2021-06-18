1b7f0683db2360558284c52fb4196dafd21d7bda
Cameron Young <Cameronslatton@gmail.com>
2021-06-17 21:35:10 -0500

    Update FileLoggerTest.cs

--- FileLoggerTests/FileLoggerTest.cs
+++ FileLoggerTests/FileLoggerTest.cs
@@ -32,5 +32,19 @@ namespace FileLoggerTests
 
             Assert.Contains(dateString, fileContents);
         }
+
+        [Fact]
+        public void Check_If_File_Exist_If_Not_Create_And_Append()
+        {
+            var IfFileExistMessage = File.Exists("log.txt");
+
+            var log = new FileLogger.FileLogger();
+            var msg = Guid.NewGuid().ToString();
+            string dateString = DateTime.Today.ToString("yyyy-MM-dd");
+
+            log.Log(msg);
+
+            var fileContents = File.ReadAllText("log.txt");
+        }
     }
 }
