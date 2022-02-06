2dbe9b26b73b2f86937ae5c01945cf3c0155500f
Cameron Young <Cameronslatton@gmail.com>
2022-02-06 08:28:15 -0600

    Write passing test

--- FileLogger/FileLogger.cs
+++ FileLogger/FileLogger.cs
@@ -1,10 +1,14 @@
-﻿namespace FileLogger
+﻿using System.IO;
+
+namespace FileLogger
 {
     public class FileLogger
     {
         public void Log(string message)
         {
-
+            using var writer = File.AppendText("log.txt");
+            
+            writer.WriteLine(message);
         }
     }
 }
